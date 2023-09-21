cad8cf0804a05314437b1ec8d03aa948bc80ed0d
motorfunction <142831513+motorfunction@users.noreply.github.com>
2023-09-21 15:30:00 -0400

    [frontend] create branch

--- opencti-platform/opencti-front/src/__tests__/selenium/analyses.test.tsx
+++ opencti-platform/opencti-front/src/__tests__/selenium/analyses.test.tsx
@@ -1,11 +1,8 @@
 import 'chromedriver';
-import { By } from 'selenium-webdriver';
 import DriverService from './common/driver_service';
 import {
-  getElementWithTimeout,
-  getSubElementWithTimeout,
-  wait,
   getXpathNodeWith,
+  wait,
 } from './common/action_service';
 import {
   addAnalysesReport,
@@ -16,10 +13,13 @@ import {
 import { deleteDomainObject } from './common/domain_object_service';
 import { logIn_LocalStrategy } from './common/auth_service';
 
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
 describe('Analyses Workflow', () => {
   const NAME = 'Test Analyses Report';
   const NEW_NAME = 'UPDATED Test Analyses Report';
   const DESCRIPTION = 'Test Analyses Report Description';
+  const NEW_DESCRIPTION = 'UPDATED Test Analysis Description';
 
   beforeAll(async () => {
     await logIn_LocalStrategy();
@@ -33,61 +33,51 @@ describe('Analyses Workflow', () => {
   test('create an analyses report', async () => {
     await navigateToAnalysesReport();
     await addAnalysesReport(NAME, DESCRIPTION);
-    await wait(5000);
-    await selectAnalysesReport(NAME);
-    // Check that name and description are correct
-    const nameField = await getXpathNodeWith('text', NAME);
-    const actualName = await nameField.getText();
-    expect(actualName).toBe(NAME);
-    const descriptionField = await getSubElementWithTimeout(
-      'id',
-      'case-analyses-report-description',
-      'p',
-    );
-    const actualDescription = await descriptionField.getText();
-    expect(actualDescription).toBe(DESCRIPTION);
   });
 
   test('view an analyses report', async () => {
-    await navigateToAnalysesReport();
-    await selectAnalysesReport(NAME);
-    await wait(6000);
+    try {
+      await navigateToAnalysesReport();
+      await selectAnalysesReport(NAME);
+      await wait(6000);
+
+      // check the name
+      await getXpathNodeWith('text', NAME)
+        .then((elem) => elem.getText())
+        .then((val) => expect(val).toBe(NAME));
+
+      // check the description
+      await getXpathNodeWith('text', DESCRIPTION)
+        .then((elem) => elem.getText())
+        .then((val) => expect(val).toBe(DESCRIPTION));
+    } catch (error) {
+      console.error('Unable to view created analyses report');
+      throw error;
+    }
   });
 
   test('edit an analyses report', async () => {
-    const NEW_DESCRIPTION = 'UPDATED Test Analysis Description';
     await editAnalysesReport(NEW_NAME, NEW_DESCRIPTION);
     await selectAnalysesReport(NEW_NAME);
-    // Check that all fields are updated correctly
-    const newNameField = await getXpathNodeWith('text', NEW_NAME);
-    const newActualName = await newNameField.getText();
-    expect(newActualName).toBe(NEW_NAME);
-    const newDescriptionField = await getSubElementWithTimeout(
-      'id',
-      'case-analyses-report-description',
-      'p',
-    );
-    const newActualDescription = await newDescriptionField.getText();
-    expect(newActualDescription).toBe(NEW_DESCRIPTION);
   });
 
   test('delete an analyses report', async () => {
-    await navigateToAnalysesReport();
-    await selectAnalysesReport(NEW_NAME);
-    await wait(5000);
-    // Need to pass in delete button ID or ERROR: Variable "$id" of non-null type "ID!" must not be null.
-    await deleteDomainObject('delete-report-button');
-    await wait(5000);
-    // Check Analyses Report no longer exists no longer shows up
-    const t = async () => {
-      await getElementWithTimeout(
-        By.xpath(`//*[text()="${NEW_NAME}"]/ancestor::a`),
-        4000,
-      );
-    };
+    try {
+      await navigateToAnalysesReport();
+      // if the report can't be found then this will throw error properly
+      expect(await selectAnalysesReport(NEW_NAME)).toBeUndefined();
+      await wait(5000);
 
-    // RxJS instanceof TimeoutError expects TimeoutErrorImpl for some reason
-    // await expect(t).rejects.toThrow(TimeoutError);
-    await expect(t).rejects.toThrow();
+      // delete the report
+      await deleteDomainObject('delete-report-button');
+      await wait(5000);
+
+      // Check Analyses Report no longer exists no longer shows up
+      getXpathNodeWith('aria-label', NEW_NAME)
+        .then((elem) => expect(elem).toBeNull());
+    } catch (error) {
+      console.error('Unable to delete the created analyses report');
+      throw error;
+    }
   });
 });
