7eee0109be3254092150feeef4114f895fc7c7b1
Verbose Cat <166016581+VerboseCat@users.noreply.github.com>
2024-04-15 16:16:21 -0400

    Add 'view as' dropdown to individual threat actor

--- opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/Root.tsx
+++ opencti-platform/opencti-front/src/private/components/threats/threat_actors_individual/Root.tsx
@@ -2,7 +2,7 @@
 // TODO Remove this when V6
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useState } from 'react';
 import { Link, Route, Routes, useParams, useLocation, Navigate, useNavigate } from 'react-router-dom';
 import { graphql, usePreloadedQuery, useSubscription } from 'react-relay';
 import { GraphQLSubscriptionConfig } from 'relay-runtime';
