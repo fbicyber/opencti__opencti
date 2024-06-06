5da1cebee22e60a3fe3e649503507fa2472f35cf
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2024-06-06 09:24:37 -0400

    [frontend] Need to test to make sure that this is to spec

--- opencti-platform/opencti-front/src/private/components/observations/stix_cyber_observables/StixCyberObservableCreation.jsx
+++ opencti-platform/opencti-front/src/private/components/observations/stix_cyber_observables/StixCyberObservableCreation.jsx
@@ -400,7 +400,6 @@ const StixCyberObservableCreation = ({
       }
     }
     function updateProgress(position, batchSize) {
-      console.log('Reached updateProgress');
       if (position % batchSize === 0) {
         setProgressBar((prevProgress) => prevProgress + 1);
       }
