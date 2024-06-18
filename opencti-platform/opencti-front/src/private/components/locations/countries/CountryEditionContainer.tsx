6ddbf27a35c36769ca6886e60d95146c32cebb15
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-06-18 12:42:43 -0400

    [frontend] Country edit FAB replaced

--- opencti-platform/opencti-front/src/private/components/locations/countries/CountryEditionContainer.tsx
+++ opencti-platform/opencti-front/src/private/components/locations/countries/CountryEditionContainer.tsx
@@ -6,11 +6,13 @@ import CountryEditionOverview from './CountryEditionOverview';
 import { CountryEditionContainerQuery } from './__generated__/CountryEditionContainerQuery.graphql';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
 import { useIsEnforceReference } from '../../../../utils/hooks/useEntitySettings';
+import useHelper from '../../../../utils/hooks/useHelper';
 
 interface CountryEditionContainerProps {
   handleClose: () => void
   queryRef: PreloadedQuery<CountryEditionContainerQuery>
   open?: boolean
+  controlledDial?: ({ onOpen }: { onOpen: () => void }) => JSX.Element
 }
 
 export const countryEditionQuery = graphql`
@@ -25,17 +27,25 @@ export const countryEditionQuery = graphql`
   }
 `;
 
-const CountryEditionContainer: FunctionComponent<CountryEditionContainerProps> = ({ handleClose, queryRef, open }) => {
+const CountryEditionContainer: FunctionComponent<CountryEditionContainerProps> = ({
+  handleClose,
+  queryRef,
+  open,
+  controlledDial,
+}) => {
   const { t_i18n } = useFormatter();
+  const { isFeatureEnable } = useHelper();
+  const FABReplaced = isFeatureEnable('FAB_REPLACEMENT');
   const { country } = usePreloadedQuery(countryEditionQuery, queryRef);
   if (country) {
     return (
       <Drawer
         title={t_i18n('Update an country')}
-        variant={open == null ? DrawerVariant.update : undefined}
+        variant={!FABReplaced && open == null ? DrawerVariant.update : undefined}
         context={country.editContext}
         onClose={handleClose}
         open={open}
+        controlledDial={FABReplaced ? controlledDial : undefined}
       >
         {({ onClose }) => (
           <CountryEditionOverview
