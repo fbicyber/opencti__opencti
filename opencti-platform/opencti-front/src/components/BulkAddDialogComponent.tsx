f4969460771c0463bb13addc5c36ed5383f1af96
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2024-06-27 13:25:50 -0400

    [frontend] All working, visual needs work, but pending redesign

--- opencti-platform/opencti-front/src/components/BulkAddDialogComponent.tsx
+++ opencti-platform/opencti-front/src/components/BulkAddDialogComponent.tsx
@@ -1,115 +1,120 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable react/jsx-indent-props */
-/* eslint-disable @typescript-eslint/indent */
 import DialogContent from '@mui/material/DialogContent';
-import IconButton from '@mui/material/IconButton';
 import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import React, { useEffect } from 'react';
 import { Field } from 'formik';
-import { TextFieldsOutlined } from '@mui/icons-material';
 import { MenuItem } from '@mui/material';
 import Select from '@mui/material/Select';
 import { useFormatter } from './i18n';
 import TextField from './TextField';
 
 type BulkAddDialogComponentProps = {
-    openBulkAddDialog: boolean
-    bulkValueFieldValue: string
-    handleOpenBulkAddDialog: () => void
-    handleCloseBulkAddDialog: (val: any) => void
-    handleParentSelectAttribute: (val: any) => void
+  openBulkAddDialog: boolean
+  bulkValueFieldValue: string
+  handleOpenBulkAddDialog: () => void
+  handleCloseBulkAddDialog: (val: any) => void
+  handleParentSelectAttribute: (val: any) => void
 };
 
 const BulkAddDialogComponent: React.FC<BulkAddDialogComponentProps> = ({
-    openBulkAddDialog,
-    bulkValueFieldValue,
-    handleOpenBulkAddDialog,
-    handleCloseBulkAddDialog,
-    handleParentSelectAttribute,
+  openBulkAddDialog,
+  bulkValueFieldValue,
+  handleOpenBulkAddDialog,
+  handleCloseBulkAddDialog,
+  handleParentSelectAttribute,
 }) => {
-    const { t_i18n } = useFormatter();
-    const [localBulkValueField, setLocalBulkValueField] = React.useState(['']);
-    const [selectedLocalAttribute, setSelectedLocalAttribute] = React.useState('');
-    const [warningVisible, setWarningVisible] = React.useState(false);
+  const { t_i18n } = useFormatter();
+  const [localBulkValueField, setLocalBulkValueField] = React.useState(['']);
+  const [selectedLocalAttribute, setSelectedLocalAttribute] = React.useState('');
+  const [warningVisible, setWarningVisible] = React.useState(false);
 
-    function monitorBulkValue(value: string) {
-        const observable_lines = value.split('\n');
-        // 50 lines have been entered based on \n newlines - warn user and remove Continue button.
-        if (observable_lines.length >= 51) {
-            if (warningVisible === false) {
-                setWarningVisible(true);
-            }
-        } else if (warningVisible === true) {
-            setWarningVisible(false);
-        }
+  function monitorBulkValue(value: string[]) {
+    const observable_lines = value.toString().split('\n');
+    // 50 lines have been entered based on \n newlines - warn user and remove Continue button.
+    if (observable_lines.length >= 51) {
+      if (warningVisible === false) {
+        setWarningVisible(true);
+      }
+    } else if (warningVisible === true) {
+      setWarningVisible(false);
     }
-    const handleSelectChange = (event: any) => {
-        setSelectedLocalAttribute(event.target.value);
-        console.log('value is: ', event.target.value);
-        console.log('LocalSelectAttribute is: ', selectedLocalAttribute);
-    };
-    useEffect(() => {
-        setLocalBulkValueField([bulkValueFieldValue]);
-    }, [bulkValueFieldValue]);
-    console.log('openBulkAddDialog is: ', openBulkAddDialog);
-    return (
-        <React.Fragment>
-            <IconButton
-                onClick={handleOpenBulkAddDialog}
-                size="large"
-                color="primary" style={{ float: 'left', marginRight: 25 }}
-            >
-                <TextFieldsOutlined />
-            </IconButton>
-            <Dialog
-                PaperProps={{ elevation: 3 }}
-                open={openBulkAddDialog}
-                onClose={handleCloseBulkAddDialog}
-                fullWidth={true}
-            >
-                <DialogContent style={{ marginTop: 0, paddingTop: 10 }}>
-                    <div id="divSelectAttributes" style={{ border: '2px solid #FFA500', paddingLeft: 10 }}>
-                        {t_i18n('Create Entities from multiple: ')}
-                        <Select name="attributes" id="attributes" value={selectedLocalAttribute} onChange={handleSelectChange}>
-                            <MenuItem selected disabled>Select attribute</MenuItem>
-                            <MenuItem value="NAME">name</MenuItem>
-                            <MenuItem value="MD5">md5</MenuItem>
-                            <MenuItem value="SHA-1">sha1</MenuItem>
-                            <MenuItem value="SHA-256">sha256</MenuItem>
-                            <MenuItem value="SHA-512">sha512</MenuItem>
-                        </Select>
-                    </div>
-                    <Typography style={{ float: 'left', marginTop: 10 }}>
-                        <span style={{ fontSize: '0.7em' }}>{selectedLocalAttribute}</span>
-                        <span className="output"></span>
-                    </Typography>
-                    <Field
-                        component={TextField}
-                        id="bulk_hashes_field"
-                        variant="standard"
-                        key="bulk_hashes_field"
-                        value={localBulkValueField}
-                        name="bulk_hashes_field"
-                        fullWidth={true}
-                        multiline={true}
-                        rows="5"
-                        onChange={(name: any, value: any) => { setLocalBulkValueField(value); monitorBulkValue(value); }}
-                    />
-                    {warningVisible
-                        && (<div style={{ color: 'red' }}>{t_i18n('Remove values or please upload them through')} <a href='/dashboard/data/import'>{t_i18n('Imports')}</a>.</div>)}
-                    <DialogActions>
-                        <Button color="secondary" onClick={() => { handleCloseBulkAddDialog(localBulkValueField); handleParentSelectAttribute(selectedLocalAttribute); }}>
-                            {t_i18n('Validate')}
-                        </Button>
-                    </DialogActions>
-                </DialogContent>
-            </Dialog>
-        </React.Fragment>
-    );
+  }
+  const handleSelectChange = (event: any) => {
+    setSelectedLocalAttribute(event.target.value);
+  };
+  useEffect(() => {
+    setLocalBulkValueField([bulkValueFieldValue]);
+  }, [bulkValueFieldValue]);
+  return (
+    <React.Fragment>
+      <Button
+        onClick={handleOpenBulkAddDialog}
+        variant={'outlined'}
+        size={'small'}
+        aria-label={'add_multiple_values_button'}
+        aria-labelledby={'add_multiple_values_button'}
+        style={{ float: 'right', marginRight: 5, marginTop: 10 }}
+      >
+        {t_i18n('Add Multiple Values')}
+      </Button>
+
+      <Dialog
+        PaperProps={{ elevation: 3 }}
+        fullWidth={true}
+        open={openBulkAddDialog}
+        onClose={handleCloseBulkAddDialog}
+      >
+        <DialogTitle>{t_i18n('Add Multiple Observable Values')}</DialogTitle>
+        <DialogContent style={{ marginTop: 0, paddingTop: 10 }}>
+          <div id="divSelectAttributes" style={{ border: '2px solid #FFA500', paddingLeft: 10 }}>
+            {t_i18n('Create Entities from multiple: ')}
+            <Select name="attributes" id="attributes" value={selectedLocalAttribute} onChange={handleSelectChange}>
+              <MenuItem selected disabled>Select attribute</MenuItem>
+              <MenuItem value="NAME">name</MenuItem>
+              <MenuItem value="MD5">md5</MenuItem>
+              <MenuItem value="SHA-1">sha1</MenuItem>
+              <MenuItem value="SHA-256">sha256</MenuItem>
+              <MenuItem value="SHA-512">sha512</MenuItem>
+            </Select>
+          </div>
+          <Typography style={{ float: 'left', marginTop: 10 }}>
+            <span style={{ fontSize: '0.7em' }}>{selectedLocalAttribute}</span>
+            <span className="output"></span>
+          </Typography>
+          <Typography variant="subtitle1" style={{ whiteSpace: 'pre-line' }}>
+            <div style={{ fontSize: '13px', paddingBottom: '20px' }}>
+              {t_i18n('Enter one observable per line. Observables must be the same type.')}
+              <br></br>
+              {t_i18n('If you are adding more than 50 values, please upload them through')} <a href='/dashboard/data/import'>{t_i18n('Imports')}</a>.
+            </div>
+          </Typography>
+          <Field
+            component={TextField}
+            id="bulk_hashes_field"
+            label={t_i18n('Multiple Values')}
+            variant="outlined"
+            value={localBulkValueField}
+            key="bulk_hashes_field"
+            name="bulk_hashes_field"
+            fullWidth={true}
+            multiline={true}
+            rows="5"
+            onChange={(name: string, value: string[]) => { setLocalBulkValueField(value); monitorBulkValue(value); }}
+          />
+          {warningVisible
+            && (<div style={{ color: 'red' }}>{t_i18n('Remove values or please upload them through')} <a href='/dashboard/data/import'>{t_i18n('Imports')}</a>.</div>)}
+          <DialogActions>
+            {!warningVisible && (<Button color="secondary" onClick={() => { handleCloseBulkAddDialog(localBulkValueField); handleParentSelectAttribute(selectedLocalAttribute); }}>
+              {t_i18n('Validate')}
+            </Button>)}
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
+  );
 };
 
 export default BulkAddDialogComponent;
