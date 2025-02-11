98603bba86cec59f6a9dcb8c6c10f21548b9f1bd
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2025-02-11 12:26:37 -0500

    [frontend] Rebased to 6.6.0; refactored

--- opencti-platform/opencti-front/src/private/components/settings/metrics/Metrics.tsx
+++ opencti-platform/opencti-front/src/private/components/settings/metrics/Metrics.tsx
@@ -1,26 +1,9 @@
-/*
-Copyright (c) 2021-2024 Filigran SAS
-
-This file is part of the OpenCTI Enterprise Edition ("EE") and is
-licensed under the OpenCTI Enterprise Edition License (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-https://github.com/OpenCTI-Platform/opencti/blob/master/LICENSE
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 import React, { FunctionComponent } from 'react';
 import Grid from '@mui/material/Grid';
 import EnterpriseEdition from '@components/common/entreprise_edition/EnterpriseEdition';
-import { graphql, useFragment } from 'react-relay';
 import AuditsMultiLineChart from '@components/common/audits/AuditsMultiLineChart';
 import AuditsHorizontalBars from '@components/common/audits/AuditsHorizontalBars';
 import Breadcrumbs from '../../../../components/Breadcrumbs';
-import ActivityMenu from '../ActivityMenu';
 import { useFormatter } from '../../../../components/i18n';
 import useEnterpriseEdition from '../../../../utils/hooks/useEnterpriseEdition';
 import { SETTINGS_SECURITYACTIVITY } from '../../../../utils/hooks/useGranted';
@@ -28,14 +11,10 @@ import Security from '../../../../utils/Security';
 import { monthsAgo } from '../../../../utils/Time';
 import Loader, { LoaderVariant } from '../../../../components/Loader';
 
-
-
 const MetricsComponent: FunctionComponent = () => {
   const { t_i18n } = useFormatter();
   const isEnterpriseEdition = useEnterpriseEdition();
 
-
-  
   if (!isEnterpriseEdition) {
     return <EnterpriseEdition feature={'User activity'} />;
   }
@@ -46,16 +25,14 @@ const MetricsComponent: FunctionComponent = () => {
     >
       <div style={{
         margin: 0,
-        padding: '0 200px 50px 0',
       }}
       >
-        <ActivityMenu />
         <Breadcrumbs elements={[{ label: t_i18n('Settings') }, { label: t_i18n('Metrics'), current: true }]} />
         <Grid container={true} spacing={3}>
           <Grid item xs={6}>
             <AuditsMultiLineChart
               height={300}
-              // startDate={monthsAgo(1)} // need to convert AuditsMultiLineChart from .jsx to .tsx component in order to pass prop..? 
+              // startDate={monthsAgo(1)} // need to convert AuditsMultiLineChart from .jsx to .tsx component in order to pass prop..?
                                           // currently getting "cannot assign string to type 'null | undefined'"
               parameters={{
                 title: t_i18n('Logins to the platform'),
@@ -81,7 +58,6 @@ const MetricsComponent: FunctionComponent = () => {
                 },
               ]}
             />
-            
           </Grid>
           <Grid item xs={6}>
             <AuditsHorizontalBars
@@ -99,7 +75,7 @@ const MetricsComponent: FunctionComponent = () => {
                     filters: [
                       {
                         key: 'members_user',
-                        values: ['88ec0c6a-13ce-5e39-b486-354fe4a7084f', '2c821418-98f1-4194-bd63-592a0d9a3500'], // hard-coded 
+                        values: ['88ec0c6a-13ce-5e39-b486-354fe4a7084f', '2c821418-98f1-4194-bd63-592a0d9a3500'], // hard-coded
                       },
                       {
                         key: 'event_scope',
@@ -109,43 +85,20 @@ const MetricsComponent: FunctionComponent = () => {
                     filterGroups: [],
                   },
                   number: 20,
-                },                
-                // {
-                //   attribute: 'context_data.id',
-                //   date_attribute: 'created',
-                //   filters: {
-                //     mode: 'and',
-                //     filters: [
-                //       {
-                //         key: 'members_user',
-                //         values: ['2c821418-98f1-4194-bd63-592a0d9a3500'], // hard-coded to test user
-                //       },
-                //       {
-                //         key: 'event_scope',
-                //         values: ['login'],
-                //       },
-                //     ],
-                //     filterGroups: [],
-                //   },
-                //   number: 20,
-                // },
+                },
               ]}
             />
           </Grid>
         </Grid>
       </div>
     </Security>
-
   );
 };
 
 const Metrics = () => {
-//   const queryRef = useQueryLoading<ConfigurationQuery>(configurationQuery, {});
-//   return queryRef ? (
   return (
     <React.Suspense fallback={<Loader variant={LoaderVariant.inElement} />}>
       <MetricsComponent />
-      {/* queryRef={queryRef} */}
     </React.Suspense>
   );
 };
