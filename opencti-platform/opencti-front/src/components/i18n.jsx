8ea3c02d4e17bf8fd85d87eb58cc1a5a1629f410
daimyo007 <165801009+daimyo007@users.noreply.github.com>
2025-04-17 14:41:52 -0400

    [frontend] removed code in comment

--- opencti-platform/opencti-front/src/components/i18n.jsx
+++ opencti-platform/opencti-front/src/components/i18n.jsx
@@ -375,15 +375,6 @@ export const useFormatter = () => {
       return '-';
     }
     // the following few lines get the date to display as the number of weeks into the year with weeks starting on Mondays
-    // const entryDate = new Date(date)
-    // const startOfYear = new Date(entryDate.getFullYear(), 0, 1);
-    // const startDayOfWeek = startOfYear.getDay();
-    // const offset = (startDayOfWeek === 0 ? 6 : (startDayOfWeek - 1)) //Adjust for Monday as start of week
-    // startOfYear.setDate(startOfYear.getDate() - offset)
-    // const diffInMilliseconds = entryDate - startOfYear;
-    // const millisecondsPerWeek = 7 * 24 * 60 * 60 * 1000;
-    // const weeksIntoYear = Math.floor(diffInMilliseconds / millisecondsPerWeek)
-    // return ("" + startOfYear.getFullYear() + "Week" + weeksIntoYear + ", " + entryDate.toLocaleString('en-US', {year: "2-digit", month: "long", day: "numeric"}))
     const value = new Date(date);
     const weekStart = startOfWeek(value, { weekStartsOn: 1 });
     const weekEnd = new Date(weekStart);
