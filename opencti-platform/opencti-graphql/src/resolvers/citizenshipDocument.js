f6339745ba3210fc11b6bc24bddbaacdcadd600f
Camillo A <camillo.agrippa.org@gmail.com>
2026-08-31 14:38:40 -0400

    issue-17288: added new files for SDO citizen doc.

---
+++ opencti-platform/opencti-graphql/src/resolvers/citizenshipDocument.js
@@ -0,0 +1,39 @@
+import { addCitizenshipDocument, findCitizenshipDocumentPaginated, findById, isUser, partOfOrganizationsPaginated } from '../domain/citizenshipDocument';
+import {
+  stixDomainObjectAddRelation,
+  stixDomainObjectCleanContext,
+  stixDomainObjectDelete,
+  stixDomainObjectDeleteRelation,
+  stixDomainObjectEditContext,
+  stixDomainObjectEditField,
+} from '../domain/stixDomainObject';
+import { ENTITY_TYPE_IDENTITY_CITIZENSHIP_DOCUMENT } from '../schema/stixDomainObject';
+
+// TODO REVIEW
+
+const citizenshipDocumentResolvers = {
+  Query: {
+    individual: (_, { id }, context) => findById(context, context.user, id),
+    individuals: (_, args, context) => findCitizenshipDocumentPaginated(context, context.user, args),
+  },
+  Individual: {
+    organizations: (individual, args, context) => partOfOrganizationsPaginated(context, context.user, individual.id, args),
+    isUser: (individual, _, context) => isUser(context, individual),
+  },
+  Mutation: {
+    individualEdit: (_, { id }, context) => ({
+      delete: async () => {
+        // Use the type-checking version that validates the entity type
+        return stixDomainObjectDelete(context, context.user, id, ENTITY_TYPE_IDENTITY_CITIZENSHIP_DOCUMENT);
+      },
+      fieldPatch: ({ input, commitMessage, references }) => stixDomainObjectEditField(context, context.user, id, input, { commitMessage, references }),
+      contextPatch: ({ input }) => stixDomainObjectEditContext(context, context.user, id, input),
+      contextClean: () => stixDomainObjectCleanContext(context, context.user, id),
+      relationAdd: ({ input }) => stixDomainObjectAddRelation(context, context.user, id, input),
+      relationDelete: ({ toId, relationship_type: relationshipType }) => stixDomainObjectDeleteRelation(context, context.user, id, toId, relationshipType),
+    }),
+    individualAdd: (_, { input }, context) => addCitizenshipDocument(context, context.user, input),
+  },
+};
+
+export default citizenshipDocumentResolvers;
